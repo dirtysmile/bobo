e20431b9fadd665b76824014544fc406be6e837f
DESKTOP-MOA9R2M\thkim <dirtysmile89@gmail.com>
2020-12-29 20:45:41 +0900

    bobo 이동

---
+++ bobo/src/components/App.js
@@ -0,0 +1,18 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="top">
+        <div className="logo">logo</div>
+        <div>header</div>
+      </div>
+      <div className="middle">
+        <div>leftmenu</div>
+        <div>content</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
